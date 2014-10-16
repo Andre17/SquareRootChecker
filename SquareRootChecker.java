46cdaaf1963842386a5467fa98af04e902d8d6db
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro.local>
2014-10-16 14:11:47 +0100

    Sqrt Checker (André Purits)

--- SquareRootChecker.java
+++ SquareRootChecker.java
@@ -2,15 +2,27 @@
 *
 * Prompts user to enter a value, checks if negative -- if not, prints out the square root
 *
-* @author <your name here>
-* @version <date you finished the code>
+* @author <AndrePurits>
+* @version <8th October 2014>
 *
 */
-
+import java.util.Scanner;
 public class SquareRootChecker {
 	
 	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
+   Scanner scnr = new Scanner(System.in);
+   System.out.println("Choose a value.");
+   double value = scnr.nextDouble();
+   if (value >= 0) {
+   System.out.println("The square root of the given value is " + Math.sqrt(value) + ".");
+   } else { 
+   System.out.println("The square root of the given value is a nonreal answer.");
+   }
 
+   return;
+   }
 }
+
+	
+
+
